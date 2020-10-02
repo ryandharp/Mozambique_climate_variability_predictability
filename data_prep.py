11f7298403e2fa3ee017cc28b90d9465f7db007e
Ryan Harp <ryan.harp@northwestern.edu>
2020-10-02 17:35:01 -0600

    test commit for PyCharm compatibility

---
+++ data_prep.py
@@ -0,0 +1,17 @@
+"""
+This script is created to subset climate data for further processing.
+
+Created 10/2/2020 by Ryan Harp.
+"""
+
+
+# Importing Modules
+
+import xarray as xr
+import matplotlib.pyplot as plt
+import dask
+
+
+# Testing Script
+plt.plot(range(5), range(5))
+plt.show()
